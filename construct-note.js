cee509cbdf5c880530a15d7a93bc3549b888bc5f
000xzhou <xzhou92@gmail.com>
2024-06-10 17:22:20 -0400

    constructNote

--- construct-note.js
+++ construct-note.js
@@ -1,10 +1,13 @@
 // add whatever parameters you deem necessary
 function constructNote(message, letters) {
+  // no letters no message
   if (letters.length === 0) return false;
 
+  // get count of the char for each
   const messageCount = countletters(message);
   const lettersCount = countletters(letters);
 
+  // loop though the char and make sure message didn't extend letters
   for (let char of messageCount.keys()) {
     if (messageCount.get(char) > lettersCount.get(char)) {
       return false;
@@ -12,9 +15,13 @@ function constructNote(message, letters) {
   }
   return true;
 }
+
 const countletters = (word) => {
   let count = new Map();
+
+  // loop though each char and add it to map if it don't exist
   for (let char of word) {
+    // count.set(char, (count.get(char) || 0) + 1);    //I feel this is hard to read
     if (count.has(char)) {
       count.set(char, count.get(char) + 1);
     } else {
