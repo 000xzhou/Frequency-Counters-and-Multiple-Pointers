d7fec1e985706637ce4fa63bed0fb8d2d6f8f6c6
000xzhou <xzhou92@gmail.com>
2024-06-10 23:03:10 -0400

    isSubsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,33 @@
-// add whatever parameters you deem necessary
-function isSubsequence() {
+/**
+ * Write a function called isSubsequence which takes in two strings and
+ * checks whether the characters in the first string form a subsequence of the characters in the second string.
+ * In other words, the function should check whether the characters in the first string appear somewhere in the second string,
+ * without their order changing.
+ *
+ * Constraints:
+ * Time Complexity - O(N + M)
+ */
+function isSubsequence(str1, str2) {
+  let i = 0;
+  let j = 0;
+
+  // Iterate through str2 (since it's the longer one)
+  while (j < str2.length) {
+    // If characters match, move both pointers
+    if (str1[i] === str2[j]) {
+      i++;
+    }
+    j++;
+
+    // If all characters of str1 are found in str2 in order
+    if (i === str1.length) {
+      return true;
+    }
+  }
+  return false;
 }
+
+isSubsequence("hello", "hello world"); // true
+isSubsequence("sing", "sting"); // true
+isSubsequence("abc", "abracadabra"); // true
+isSubsequence("abc", "acb"); // false (order matters)
