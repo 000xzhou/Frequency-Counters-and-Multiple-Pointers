e9915208644d9642e4ebaae2c373981c05de5c96
000xzhou <xzhou92@gmail.com>
2024-06-10 22:46:23 -0400

    separatePositive. change the arr[3] x3 to arr[3] 4 5

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,29 @@
-// add whatever parameters you deem necessary
-function separatePositive() {
+/**
+ * Write a function called separatePositive which accepts an array of non-zero integers.
+ * Separate the positive integers to the left and the negative integers to the right.
+ * The positive numbers and negative numbers need not be in sorted order.
+ * The problem should be done in place (in other words, do not build a copy of the input array).
+ *
+ * Constraints
+ * Time Complexity: O(N)
+ */
+function separatePositive(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    if (arr[left] > 0) {
+      left++;
+    } else if (arr[right] < 0) {
+      right--;
+    } else if (arr[right] > 0 || arr[left] < 0) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+    }
+  }
+  return arr;
 }
+
+separatePositive([2, -1, -3, 6, -8, 10]); // [2, 10, 6, -3, -1, -8]
+separatePositive([5, 10, -15, 20, 25]); // [5, 10, 25, 20, -15]
+separatePositive([-5, 5]); // [5, -5]
+separatePositive([1, 2, 3]); // [1, 2, 3]
